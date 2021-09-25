c7b1334f9d66c32a4f2c0c426cd9c908c9beffa0
edwin rodriguez balcazares <cuadrado1980@yahoo.es>
2021-09-25 18:15:48 -0500

    V1.0.0 Inicio del Proyecto

---
+++ Ganaderia.App.Consola/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
+
+namespace Ganaderia.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioGanadero _repositorioGanadero = new RepositorioGanadero(new Persistencia.AppContext()); 
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddGanadero();
+            
+        }
+        private static void AddGanadero()
+        {
+            var ganadero = new Ganadero
+            {
+                Nombres = "Edwin",
+                Apellidos = "Rodriguez",
+                NumeroTelefono = "3148596562",
+                Correo = "edwin.rodriguez@cun.edu.co",
+                Contrasena = "12345",
+                Latitud = 4554,
+                Longitud = 5454,
+            };
+
+            _repositorioGanadero.AddGanadero(ganadero);
+
+
+        }
+        
+    }
+}
---
+++ Ganaderia.App.Dominio/Entidades/Veterinario.cs
@@ -0,0 +1,11 @@
+namespace Ganaderia.App.Dominio
+{
+    public class Veterinario : Persona
+    {
+        public float Latitud { get; set; }
+        public float Longitud { get; set; }
+        public float TargetaProfesional { get; set; }
+        public float Especialidad { get; set; }
+        
+    }
+}
